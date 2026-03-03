c1bdce7c4551d33c14a4d90197d3268d467a50b8
José Mogollón <jlmogollon@outlook.com>
2026-03-03 22:28:41 +0100

    Revert 'src/App.jsx' to commit 04f41515ae1e1918f8fd8354d2b25a113ab090ab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-// Previous version of src/App.jsx as of commit 04f41515ae1e1918f8fd8354d2b25a113ab090ab
+// Reverted content from commit 04f41515ae1e1918f8fd8354d2b25a113ab090ab
 
-import React from 'react';
-
-function App() {
-    return (
-        <div>
-            <h1>Welcome to Kids Goals!</h1>
-        </div>
-    );
-}
-
-export default App;
+// Previous content of src/App.jsx
